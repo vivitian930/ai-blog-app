fad3a2718a9221f0af0caa86530196996ec1719f
Vivi Tian <vivi.tian930@gmail.com>
2023-04-20 22:14:35 +1200

    add hearder and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Open_Sans } from "next/font/google"
+import Footer from "./(shared)/Footer"
+import Navbar from "./(shared)/Navbar"
 import "./globals.css"
 
 const openSans = Open_Sans({
@@ -17,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html className={openSans.className} lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
